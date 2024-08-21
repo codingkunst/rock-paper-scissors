e3356d2fd60647750ce19447b00ec3ebf17328cc
codingkunst <ryoo6931@gmail.com>
2024-08-21 13:20:48 +0900

    [Feat] state 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import './App.css'
-import Box from './components/Box'
+import { useState } from "react";
+import "./App.css";
+import Box from "./components/Box";
+
+const choice = {
+  rock: {
+    name: "rock",
+    image: "https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296854_960_720.png",
+  },
+  paper: {
+    name: "paper",
+    image: "https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296855_1280.png",
+  },
+  scissors: {
+    name: "scissors",
+    image: "https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296853_1280.png",
+  },
+};
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  };
 
   return (
     <div>
-      <div className='main'>
-        <Box title="나" />
-        <Box title="AI" />
+      <div className="main">
+        <Box title="나" item={userSelect} />
+        {/* <Box title="AI" /> */}
       </div>
 
-      <div className='main'>
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
