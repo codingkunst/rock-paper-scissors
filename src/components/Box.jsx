81b3221fd03596215a66344c76ad72abb873225a
codingkunst <ryoo6931@gmail.com>
2024-08-21 14:25:15 +0900

    [Design] tailwind css 적용

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,9 +4,9 @@ import '../App.css'
 const Box = (props) => {
   console.log('props : ', props);
   return (
-    <div className='box'>
+    <div className='rounded border border-gray-300 border-solid w-96'>
       <h1>{props.title}</h1>
-      <img className='item-img' src={props.item && props.item.image} />
+      <img className='w-auto h-auto' src={props.item && props.item.image} />
       <h2>win</h2>
     </div>
   )
